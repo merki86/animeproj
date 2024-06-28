8b45dc49a5ff525b0afa7e9c7d0709dd78c82242
serwennn <nassen.d230@gmail.com>
2024-06-28 21:58:00 +0500

    A lot of things

--- main.go
+++ main.go
@@ -10,15 +10,13 @@ import (
 func main() {
 	fmt.Println("Starting")
 
+	static := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/static/", http.StripPrefix("/static", static))
+
 	http.HandleFunc("/", controllers.GetAnimes)
 	http.HandleFunc("/add-anime", controllers.AddAnime)
 	http.HandleFunc("/delete-anime", controllers.DeleteAnime)
-	// http.HandleFunc("/anime/{id}", anime)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
-// func anime(w http.ResponseWriter, req *http.Request) {
-// 	idString := req.PathValue("id")
-// 	fmt.Fprintf(w, "Anime ID: %v", idString)
-// }
